890683966ec9f34fdcfd2974214fc158aa0853ce
jaylon0 <102235424+jaylon0@users.noreply.github.com>
2023-07-05 16:36:36 +0800

    提交文件

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import './assets/css/style.less'
+import store from './store'
+import MavonEditor from 'mavon-editor'
+import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
+
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(MavonEditor)
+Vue.component(CollapseTransition.name, CollapseTransition)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  store
+})
---
+++ src/pages/Share.vue
@@ -0,0 +1,47 @@
+<!-- 文章列表 -->
+<template>
+	<div>
+		<!-- <sg-nav></sg-nav> -->
+		<div class="container">
+			<el-row :gutter="30">
+				<el-col :sm="24" :md="16" style="transition:all .5s ease-out;margin-bottom:30px;">
+					<sg-articlelist></sg-articlelist>
+				</el-col>
+				<el-col :sm="24" :md="8">
+					<sg-rightlist></sg-rightlist>
+				</el-col>
+			</el-row>
+		</div>
+		<wbc-footer></wbc-footer>
+	</div>
+</template>
+
+<script>
+import header from '../components/header.vue'
+import articlelist from '../components/articlelist.vue'
+import footer from '../components/footer.vue'
+import rightlist from '../components/rightlist.vue'
+export default {
+	name: "Share",
+	data() { //选项 / 数据
+		return {
+
+		}
+	},
+	methods: { //事件处理器
+
+	},
+	components: { //定义组件
+		'sg-nav': header,
+		'sg-articlelist': articlelist,
+		'sg-rightlist': rightlist,
+		'wbc-footer': footer
+	},
+
+	created() { //生命周期函数
+
+	}
+}
+</script>
+
+<style></style>
